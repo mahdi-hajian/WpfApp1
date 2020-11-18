9cb06f41578a767f7dcda5f368e40d63b1ae5763
mahdi hajian <mahdi.haj.1375@gmail.com>
2020-11-18 14:44:39 +0330

    Add project files.

---
+++ WpfApp1/Identity/Mcontext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1.Identity
+{
+    class Mcontext: DbContext
+    {
+        public Mcontext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source=blogging.db");
+        }
+    }
+}
